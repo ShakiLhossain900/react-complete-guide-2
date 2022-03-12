84a2fe4be99389e655c6f562aead4036de3d022f
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-03-12 12:36:46 +0600

    adding something new

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ExpenseItem.css'
+
+const ExpenseItem = (props) => {
+    const title =props.title;
+    const amount=props.amount;
+    const date=props.date;
+    return (
+        <div className="expense-item">
+             <div>{date.toISOString()}</div>
+             <div className="expense-item__description">
+                 <h2>{title}</h2>
+                 <div className="expense-item__price">${amount}</div>
+             </div>
+        </div>
+    );
+};
+
+export default ExpenseItem;
