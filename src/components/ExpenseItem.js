e3f15e306908ffda36360daed8881c3d20eaa534
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-03-16 06:08:33 +0600

    adding new think

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import './ExpenseItem.css'
+import React from "react";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-    const title =props.title;
-    const amount=props.amount;
-    const date=props.date;
-    return (
-        <div className="expense-item">
-             <div>{date.toISOString()}</div>
-             <div className="expense-item__description">
-                 <h2>{title}</h2>
-                 <div className="expense-item__price">${amount}</div>
-             </div>
-        </div>
-    );
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
